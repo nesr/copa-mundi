c4bae8a020821c0d653cb5249e100cb2c9737b45
Emilce Silva <emilce.s.91@gmail.com>
2014-04-02 10:49:00 -0500

    instr

--- worldcup/urls.py
+++ worldcup/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 import equipos.views
  
@@ -8,14 +9,16 @@ admin.autodiscover()
 urlpatterns = patterns('',
 # Examples:
 # url(r'^blog/', include('blog.urls')),
-url(r'^continentes/', equipos.views.ListarContinentes.as_view(), name='lista-continentes',),
-url(r'^jugadores/', equipos.views.ListarJugadores.as_view(), name='lista-jugadores',),
-url(r'^equipos/', equipos.views.ListarEquipos.as_view(), name='lista-equipos',),
+    url(r'^continentes/', equipos.views.ListarContinentes.as_view(), name='lista-continentes',),
+    url(r'^jugadores/', equipos.views.ListarJugadores.as_view(), name='lista-jugadores',),
+    url(r'^equipos/', equipos.views.ListarEquipos.as_view(), name='lista-equipos',),
+    url(r'^player/(?P<jugador_id>\d+)/player.html$',
+		'equipos.views.detalleJugador', name = 'detalle-jugador'),
  
-url(r'^admin/', include(admin.site.urls)),
-url(r'^media/(?P<path>.*)$','django.views.static.serve',
-{'document_root':settings.MEDIA_ROOT,}
-),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',
+                {'document_root':settings.MEDIA_ROOT,}
+       ),
 )
  
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
